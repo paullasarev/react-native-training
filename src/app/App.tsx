ced9a02fb1608b1e1d7eb268901fb30891a89716
Pavel Lasarev <Pavel_Lasarev@epam.com>
2020-07-07 11:39:19 +0400

    fix app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import 'react-native-gesture-handler';
+// import { Text, View } from 'react-native';
 import MainNavigator from '../navigation/MainNavigator';
 import navigation from '../navigation';
 
-export const App = () => {
+const App = () => {
+  // return <MainNavigator />;
+  // return (
+  //   <View>
+  //     <Text>app</Text>
+  //   </View>
+  // );
   return <MainNavigator ref={navigation.init} />;
 };
+
+export default App;
