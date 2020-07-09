9c564de28f1999f8776effb3a6d5420258e26970
Pavel Lasarev <Pavel_Lasarev@epam.com>
2020-07-09 10:09:17 +0400

    fix api prefix and images serve

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,16 +1,19 @@
+const path = require('path');
 const jsonServer = require('json-server');
-// const bodyParser = require('body-parser');
+const db = require('./db.json');
+// const routes = require('./routes.json');
 
 const server = jsonServer.create();
-const router = jsonServer.router('mock-server/db.json');
-const middlewares = jsonServer.defaults({
-  static: 'public',
-});
+const router = jsonServer.router(db);
+// const rewriter = jsonServer.rewriter(routes);
+const static = path.join(__dirname, 'public');
+const middlewares = jsonServer.defaults({ static });
 const PORT = 3000;
 
+// server.use(rewriter);
 server.use(middlewares);
-// server.use(bodyParser);
-server.use(router);
+server.use('/api', router);
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`JSON Server is running on ${PORT}`);
 });
